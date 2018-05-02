b563413561ff47a29af12a0e050209039d43d9dd
yiqingye <yiqing4@illinois.edu>
2018-05-02 16:36:51 -0500

    complete

--- app/src/main/java/edu/illinois/cs/cs125/myapplication/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.net.Uri;
+
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +16,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.RequestQueue;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     private RequestQueue requestQueue;
     String TAG = "Main Activity";
     public Button button;
+    public Button buttonr;
+    public String info;
+    public int sum1 =0;
+    public int sum2 = 0;
 
     //TextView searchResult;
     @Override
@@ -43,28 +50,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("https://en.wikipedia.org/wiki/Reversi"));
+                intent.setData(Uri.parse("https://www.google.es/"));
                 startActivity(intent);
             }
         });
 
+        //startAPICall("Apple Inc.");
 
-        //searchResult = (TextView) findViewById(R.id.searchbar);
-//        final Button refreshButton = findViewById(R.id.refresher);
-//        refreshButton.setOnClickListener(new View.OnClickListener() {
+//        buttona = (Button) findViewById(R.id.button1);
+//        buttona.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onClick(final View v) {
-//                Log.d(TAG, "after click, refresh anime results");
-////                startAPICall();
+//            public void onClick(View v) {
+////                String b1 = info;
+////                TextView b2 = (TextView) findViewById(R.id.textView3);
+////                b2.setText(b1);
+//
 //            }
 //        });
     }
-//    public void buttonOnclick(View v) {
-//        Button button = (Button) v;
-//        ((Button) v).setText("complete");
 
 
-    public void buttonOnclick1(View v) {
+    public void buttonOnclick1(final View v) {
         double a = Math.random() * 10000000;
         int b = (int) a;
         Button button = (Button) v;
@@ -81,61 +87,99 @@ public class MainActivity extends AppCompatActivity {
             image1.setImageResource(R.drawable.pikachu);
         } else if (b % 2 == 0) {
             ((Button) v).setText("Try again");
+//            image1.setImageResource(R.drawable.imageTree);
         } else {
             ((Button) v).setText("Surprise");
             image1.setImageResource(R.drawable.horror);
         }
     }
-    public void doSomething(View v) {
-        Scanner input = new Scanner(System.in);
-        String anime1 = input.nextLine();
-        startAPICall("anime");
-        TextView view3 = (TextView) findViewById(R.id.textView3);
-        view3.setText()
+
+//    public void doSomething(String v) {
+//        startAPICall("Apple Inc.");
+//       // TextView view3 = (TextView) findViewById(R.id.textView3);
+//    }
+    public void buttonOnclick2(View v) {
+        int n = (int) (Math.random() * 6) + 1;
+        int m = (int) (Math.random() * 6) + 1;
+        int l = (int) (Math.random() * 6) + 1;
+        TextView ab = (TextView) findViewById(R.id.textView12);
+        TextView bc = (TextView) findViewById(R.id.textView11);
+        TextView cd = (TextView) findViewById(R.id.textView10);
+        ab.setText(""+ n);
+        bc.setText(""+ m);
+        cd.setText("" + l);
+        sum1 = n + m +l;
+
+    }
+    public void buttonOnclick3(View v) {
+        int n = (int) (Math.random() * 6) + 1;
+        int m = (int) (Math.random() * 6) + 1;
+        int l = (int) (Math.random() * 6) + 1;
+        TextView ab = (TextView) findViewById(R.id.textView21);
+        TextView bc = (TextView) findViewById(R.id.textView22);
+        TextView cd = (TextView) findViewById(R.id.textView20);
+        ab.setText(""+ n);
+        bc.setText(""+ m);
+        cd.setText("" + l);
+        sum2 = n + m + l;
+
     }
 
-//    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-//            Request.Method.GET,
-//            "http://thecatapi.com/api/",
-//            null,
-//            new Response.Listener<JSONArray>() {
-//                public void onResponse(JsonArray response) {
-//
-//                }
-//            }, new Response.ErrorListener() {
-//        @Override
-//        public void onErrorResponse(VolleyError error) {
-//            Log.w(TAG, error.toString());
-//        }
-//    });
-
-
-
-
-   void startAPICall(String anime) {
-        try{
-
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    "http://kitsu.io/api/edge" + anime,
-                    null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(final JSONObject response) {
-                            Log.d(TAG, response.toString());
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(final VolleyError error) {
-                    Log.v(TAG, error.toString());
-                }
-            });
-            requestQueue.add(jsonObjectRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void buttonClick4(View v) {
+        TextView fg = (TextView) findViewById(R.id.textView3);
+        if (sum1 > sum2) {
+             fg.setText("Player 1 Win");
+        } else if (sum1 < sum2) {
+            fg.setText("Player 2 Win");
+        } else {
+            fg.setText("No Winner");
         }
 
     }
+
+//    public String getFormat(final String json) {
+//        if (json == null) {
+//            return null;
+//        }
+//        JsonParser parser = new JsonParser();
+//        JsonObject result = parser.parse(json).getAsJsonObject();
+//        JsonObject a1 = result.getAsJsonObject("data");
+//        String a2 = a1.get("attribute").getAsString();
+//        return a2;
+//
+//    }
+
+
+
+
+//   void startAPICall(String company) {
+//        try{
+//            Log.d(TAG,"im 2");
+//            JsonObjectRequest something = new JsonObjectRequest(
+//                    Request.Method.GET,
+//                    "https://api.iextrading.com/1.0/stock/" + company + "/chart/Id",
+//                    null,
+//                    new Response.Listener<JSONObject>() {
+//                        @Override
+//                        public void onResponse(final JSONObject response) {
+////                            TextView some = (TextView) findViewById(R.id.textView3);
+////                            some.setText(response.toString());
+//                            //info = getFormat(response.toString());
+//                            Log.d(TAG, "im");
+//                        }
+//                    }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(final VolleyError error) {
+//                    Log.v(TAG, error.toString());
+//                }
+//            });
+//Log.d(TAG,"im3");
+//            requestQueue.add(something);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
 
 }
